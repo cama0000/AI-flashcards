fb53f6f51e47086dd4f6577b5bfe1a5f9c1baa2d
cama0000 <acama0000@gmail.com>
2024-08-15 13:57:53 -0500

    style sign in and sign up

--- app/sign-in/[[...sign-in]]/page.js
+++ app/sign-in/[[...sign-in]]/page.js
@@ -1,5 +1,18 @@
 import { SignIn } from "@clerk/nextjs";
+import { Box } from "@mui/material";
 
-export default function Page(){
-    return <SignIn />
+export default function Page() {
+    return (
+        <Box
+            sx={{
+                minHeight: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundImage: 'linear-gradient(to right, #ff6ec4, #7873f5)',
+            }}
+        >
+            <SignIn/>
+        </Box>
+    );
 }
--- app/sign-up/[[...sign-in]]/page.js
+++ app/sign-up/[[...sign-in]]/page.js
@@ -1,5 +1,18 @@
 import { SignUp } from "@clerk/nextjs";
+import { Box } from "@mui/material";
 
-export default function Page(){
-    return <SignUp />
+export default function Page() {
+    return (
+        <Box
+            sx={{
+                minHeight: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundImage: 'linear-gradient(to right, #ff6ec4, #7873f5)',
+            }}
+        >
+            <SignUp/>
+        </Box>
+    );
 }
