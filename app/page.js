f7c827039f7e9ce00201687d06cee99cf9009d44
cama0000 <acama0000@gmail.com>
2024-08-14 23:04:22 -0500

    test commit

--- app/page.js
+++ app/page.js
@@ -98,7 +98,7 @@ export default function Home() {
         >
           <Container>
             <Typography variant="h2" component="h1" gutterBottom>
-              Transform Your Notes into Flashcards
+              Transform Your Notes into YEAHHHH
             </Typography>
             <Typography variant="h5" component="p" gutterBottom>
               Boost your learning with our intelligent flashcard generator.
